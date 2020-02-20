b46f171ddea3be8d0124f2f89a1a34bcf4208063
Gion Kunz <Gion_Kunz@rcomext.com>
2020-02-20 18:16:15 +0100

    First steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sae';
+  person = {
+    name: 'Peter'
+  };
+  numbers = [1, 2, 3, 4, 5];
+
+  count = 0;
+
+  constructor() {
+    setInterval(() => this.count++, 1000);
+  }
+
+  getPersonName() {
+    return this.person.name;
+  }
+
+  reset(initialValue = 0) {
+    this.count = initialValue;
+  }
 }
